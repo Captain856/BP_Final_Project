428cf8803fbfc2e5df70d483c6fcd5cd955d5c2f
Captain856 <mahdialiakbari856@gmail.com>
2026-01-02 21:40:13 +0330

    mapinit and drawmap

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <math.h>
-#include "raylib.h"
+#include <raylib.h>
 #include "map.h"
-
-struct vector{
-    float x;
-    float y;
-};
 
 int main()
 {
     MapInit();
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenWidth = 960;
+    const int screenHeight = 540;
     InitWindow(screenWidth, screenHeight, "Raycaster");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawMap();
+        EndDrawing();
     }
     CloseWindow();
     
