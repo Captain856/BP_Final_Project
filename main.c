5e6f94e3768709259528c51a2ddac29b52ebdf40
Captain856 <mahdialiakbari856@gmail.com>
2026-01-17 14:27:30 +0330

    added tutorial mode

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ typedef enum
     EYE
 } GameStatus;
 
+int WelcomePage(int timer);
+
 int main()
 {
     MapInit();
@@ -20,8 +22,9 @@ int main()
     const int ScreenHeight = 540;
     InitWindow(ScreenWidth, ScreenHeight, "Prisoner of the Garden");
     SetTargetFPS(60);
-    GameStatus ViewMode = EYE;
-    int timer = 0;
+    GameStatus ViewMode = TUTORIAL;
+    int timer1 = 0;
+    int timer2 = 0;
     while (!WindowShouldClose())
     {
         if(IsKeyDown(KEY_W))MoveForward();
@@ -32,6 +35,12 @@ int main()
         if(IsKeyDown(KEY_LEFT))TurnLeft();
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        if(ViewMode == TUTORIAL)
+        {
+            timer2 = WelcomePage(timer2);
+            if(timer2>0)if(IsKeyDown(KEY_SPACE))timer2 = -1;
+            if(timer2<-60)if(IsKeyDown(KEY_SPACE))ViewMode = EYE;
+        }
         if(ViewMode == EYE)
         {
             for(int i = 1; i <= ScreenWidth; i++)
@@ -59,27 +68,55 @@ int main()
             DrawText("to remove a wall", 20, 200, 20, BLACK);
         }
         EndDrawing();
-        switch (timer)
+        switch (timer1)
         {
             case 0:
                 if(IsKeyDown(KEY_M))
                 {
                     if(ViewMode == MAP) ViewMode = EYE;
                     else ViewMode = MAP;
-                    timer = 1;
+                    timer1 = 1;
                 }
                 break;
             
             case 15:
-                timer = 0;
+                timer1 = 0;
                 break;
 
             default:
-                timer++;
+                timer1++;
                 break;
         }
     }
     CloseWindow();
     
     return 0;
+}
+
+int WelcomePage(int timer)
+{
+    ClearBackground((Color){106, 120, 48, 255});
+    if(timer >= 10 && timer <= 70) DrawText("Loading...", 803, 483, 30, (Color){0, 0, 0, 150});
+    if(timer >= 10 && timer <= 70) DrawText("Loading...", 800, 480, 30, WHITE);
+    if(timer >= 100) DrawText("PRISONER", 173, 53, 120, (Color){0, 0, 0, 150});
+    if(timer >= 100) DrawText("PRISONER", 170, 50, 120, RED);
+    if(timer >= 130) DrawText("OF THE", 413, 158, 40, (Color){0, 0, 0, 150});
+    if(timer >= 130) DrawText("OF THE", 410, 155, 40, (Color){16, 104, 32, 255});
+    if(timer >= 160) DrawText("GARDEN", 303, 193, 90, (Color){0, 0, 0, 150});
+    if(timer >= 160) DrawText("GARDEN", 300, 190, 90, (Color){16, 104, 32, 255});
+    if(timer >= 210) DrawText("press 'Space' to continue", 293, 403, 30, (Color){0, 0, 0, 150});
+    if(timer >= 210) DrawText("press 'Space' to continue", 290, 400, 30, WHITE);
+    if(timer < 0)
+    {
+        DrawText("'TUTORIAL'", 53, 53, 40, (Color){0, 0, 0, 150});
+        DrawText("'TUTORIAL'", 50, 50, 40, WHITE);
+        DrawText("use 'W', 'A', 'S' and 'D' to move in the Garden", 73, 133, 30, (Color){0, 0, 0, 150});
+        DrawText("use 'W', 'A', 'S' and 'D' to move in the Garden", 70, 130, 30, WHITE);
+        DrawText("use '<-' and '->' to turn around", 73, 173, 30, (Color){0, 0, 0, 150});
+        DrawText("use '<-' and '->' to turn around", 70, 170, 30, WHITE);
+        DrawText("when ready, press 'Space'", 73, 453, 30, (Color){0, 0, 0, 150});
+        DrawText("when ready, press 'Space'", 70, 450, 30, WHITE);
+        return timer-1;
+    }
+    return timer+1;
 }
